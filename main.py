e2141bc72a4451ae4fa8261b180e8f007a7e9826
Keigo Mega <dsb4v3@gmail.com>
2020-03-17 12:53:58 +0900

    Japanese

--- main.py
+++ main.py
@@ -14,18 +14,21 @@ def main():
     if ret:
         p = pings.Ping()
         while True:
+            i=0
             texts = ''
-            hb = 'seraching'
+            hb = '  サーチ中'
             for name, ip in ip_dict.items():
+                i+=1
                 res = p.ping(ip)
                 if not res.is_reached():
-                    texts += f'\033[41m{name} ({ip})\033[0m\n'
+                    texts += f'{i:2} \033[41m{name} ({ip})\033[0m\n'
                 else:
-                    texts += f'\033[32m{name} ({ip})\033[0m\n'
-                print(modules.nowTime(),  hb, end='\r')
+                    texts += f'{i:2} \033[32m{name} ({ip})\033[0m\n'
+                print(hb, end='\r')
                 hb += '.'
             os.system('cls')
             print('<-- 通信機器生存チェッカー -->')
+            print(modules.nowTime(), '時点での生存確認')
             print(texts)
     else:
         print('please save config.ini with IP List into inifiles...')
